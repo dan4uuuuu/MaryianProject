cc05fdf656c650e75bbb80e374fb00972bcf60d9
Yordan Ivanov <Yordan Ivanov@DESKTOP-CJOO4H7>
2018-05-03 14:38:32 +0300

    Add project files.

---
+++ MariyanProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MariyanProject.Startup))]
+namespace MariyanProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
